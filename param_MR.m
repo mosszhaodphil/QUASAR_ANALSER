ef7024c3b4627aa0b0fc560b3ff35a9fb7aa04e9
mosszhaodphil <mosszhaodphil@gmail.com>
2014-12-16 17:18:26 +0000

    MR parameter variables
    Default values of MR parameters

---
+++ param_MR.m
@@ -0,0 +1,7 @@
+
+x        = 0; % x dimension of k space
+y        = 0; % y dimension of k space
+z        = 0; % z dimension, or total number of slices
+length_t = 0; % total number of sampling points
+
+save('param_MR.mat');
